003c6e499fd059015e0070049e32dedcf10dd859
David Vuong <david.vuong@uwaterloo.ca>
2025-01-28 00:18:46 -0800

    A2 - rest of solution

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 #include <syslog.h>
 
+// Logging example: syslog(LOG_DEBUG, "Testing %d", 1000);
+
 int main(int argc, char **argv)
 {
+    FILE *ptr = NULL;
+    char *filename = argv[1];
+    char *writestr = argv[2];
+
     openlog(NULL, 0, LOG_USER);
-    syslog(LOG_DEBUG, "Testing %d", 1000);
+
+    if (argc < 3)
+    {
+        syslog(LOG_ERR, "Error - insufficient parameters passed in. Num parameters passed in: %d", argc);
+        return 1;
+    }
+
+    if ((ptr = fopen(filename, "w")) == NULL)
+    {
+        syslog(LOG_ERR, "Could not create file %s", filename);
+        return 1;
+    }
+
+    syslog(LOG_DEBUG, "Writing %s to %s", writestr, filename);
+    fprintf(ptr, "%s", writestr);
+
     return 0;
 }
