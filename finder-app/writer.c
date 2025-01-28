4eff90a82375ea8c22bb5bd8f9e881343bf830e5
David Vuong <david.vuong@uwaterloo.ca>
2025-01-28 00:00:40 -0800

    Testing syslog
    mint@linux-mint-box:~/cuboulder/ecea5305/assignment-1-davidxvuong/finder-app$ cat /var/log/syslog | grep writer
    2025-01-27T23:59:45.329755-08:00 linux-mint-box writer: Testing 1000

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <syslog.h>
 
 int main(int argc, char **argv)
 {
-    printf("Hello World!\n");
+    openlog(NULL, 0, LOG_USER);
+    syslog(LOG_DEBUG, "Testing %d", 1000);
     return 0;
 }
